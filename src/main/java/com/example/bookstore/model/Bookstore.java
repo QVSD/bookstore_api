3b8f3b16b34135f24889ac7462b79cbd05701b42
QVSD <gameruldragos@gmail.com>
2025-09-29 23:21:05 +0300

    Bookstore model added

---
+++ src/main/java/com/example/bookstore/model/Bookstore.java
@@ -0,0 +1,29 @@
+package com.example.bookstore.model;
+import jakarta.persistence.*;
+
+@Entity
+public class Bookstore{
+
+    @Id @GeneratedValue
+    private Long id;
+
+    private String title;
+    private String category;
+    private int noOfPages;
+
+    public Bookstore (){};
+
+    public Long getId(){return id;}
+    public void setId(Long id){ this.id = id;}
+
+    public String getTitle(){return title;}
+    public void setTitle(String title){ this.title = title;}
+
+    public String getCategory(){return category;}
+    public void setCategory(String catergory){this.category = catergory;}
+
+    public int getNoOfPages(){return noOfPages;}
+    public void setNoOfPages(int noOfPages){this.noOfPages = noOfPages;}
+
+}
+
